302ba3072f23d37e0036172631ceaa4faa935a58
labyu <labyu2020@gmail.com>
2020-09-16 11:37:58 +0900

    add sonarqube

--- onprem/infra/certbot/renew.sh
+++ onprem/infra/certbot/renew.sh
@@ -8,12 +8,13 @@ CERT_PATH=/etc/letsencrypt/live/apjung.me/cert.pem
 
 ARGOCD=argocd
 JENKINS=jenkins
+SONARQUBE=sonarqube
 
 # 기존 시크릿 제거
 kubectl delete secret $SECRET
 kubectl -n $ARGOCD delete secret $SECRET
 kubectl -n $JENKINS delete secret $SECRET 
-
+kubectl -n $SONARQUBE delete secret $SECRET
 
 # default에 시크릿생성
 kubectl create secret tls $SECRET --key $KEY_PATH --cert $CERT_PATH
@@ -24,8 +25,8 @@ kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kube
 # Jenkins로 시크릿복사
 kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$JENKINS -f -
 
-
-
+# SonarQube로 시크릿복사
+kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$SONARQUBE -f -
 
 
 
