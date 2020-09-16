4cf7a4bba8e63c5da4655e10c53736d74e4dd7e7
labyu <labyu2020@gmail.com>
2020-09-16 21:51:26 +0900

    commit

--- onprem/infra/certbot/renew.sh
+++ onprem/infra/certbot/renew.sh
@@ -9,12 +9,14 @@ CERT_PATH=/etc/letsencrypt/live/apjung.me/cert.pem
 ARGOCD=argocd
 JENKINS=jenkins
 SONARQUBE=sonarqube
+DEV=dev
 
 # 기존 시크릿 제거
 kubectl delete secret $SECRET
 kubectl -n $ARGOCD delete secret $SECRET
 kubectl -n $JENKINS delete secret $SECRET 
 kubectl -n $SONARQUBE delete secret $SECRET
+kubectl -n $DEV delete secret $SECRET
 
 # default에 시크릿생성
 kubectl create secret tls $SECRET --key $KEY_PATH --cert $CERT_PATH
@@ -28,5 +30,6 @@ kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kube
 # SonarQube로 시크릿복사
 kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$SONARQUBE -f -
 
-
+# Dev로 시크릿복사
+kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$DEV -f -
 
