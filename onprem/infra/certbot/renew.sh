33f20ff72f1e57219f7049611e56c19ae7bb802e
labyu <labyu2020@gmail.com>
2020-09-21 16:11:21 +0900

    commit

--- onprem/infra/certbot/renew.sh
+++ onprem/infra/certbot/renew.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-SECRET=tls-secret
+DEV=dev
+DEV_SECRET=tls-secret-dev
+DEV_KEY_PATH=/etc/letsencrypt/live/apjung.xyz/privkey.pem
+DEV_CERT_PATH=/etc/letsencrypt/live/apjung.xyz/cert.pem
 
+# 기존 시크릿 제거
+kubectl delete secret $DEV_SECRET
+kubectl -n $DEV delete secret $DEV_SECRET 
+
+# default에 시크릿생성
+kubectl create secret tls $DEV_SECRET --key $DEV_KEY_PATH --cert $DEV_CERT_PATH
+
+# Dev로 시크릿복사 
+kubectl get secret $DEV_SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$DEV -f -
+
+SECRET=tls-secret
 KEY_PATH=/etc/letsencrypt/live/apjung.me/privkey.pem
 CERT_PATH=/etc/letsencrypt/live/apjung.me/cert.pem
 
@@ -9,14 +23,12 @@ CERT_PATH=/etc/letsencrypt/live/apjung.me/cert.pem
 ARGOCD=argocd
 JENKINS=jenkins
 SONARQUBE=sonarqube
-DEV=dev
 
 # 기존 시크릿 제거
 kubectl delete secret $SECRET
 kubectl -n $ARGOCD delete secret $SECRET
 kubectl -n $JENKINS delete secret $SECRET 
 kubectl -n $SONARQUBE delete secret $SECRET
-kubectl -n $DEV delete secret $SECRET
 
 # default에 시크릿생성
 kubectl create secret tls $SECRET --key $KEY_PATH --cert $CERT_PATH
@@ -30,6 +42,4 @@ kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kube
 # SonarQube로 시크릿복사
 kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$SONARQUBE -f -
 
-# Dev로 시크릿복사
-kubectl get secret $SECRET --namespace=default -oyaml | grep -v namespace | kubectl apply --namespace=$DEV -f -
 
